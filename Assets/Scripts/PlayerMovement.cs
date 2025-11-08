68391a40a1f377df3775979cd3b0235b6567c425
BlackOrangee <33674663+BlackOrangee@users.noreply.github.com>
2025-11-08 18:44:12 +0200

    add enemy AI with vision and hearing support

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,4 +1,4 @@
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace Assets.Scripts
 {
@@ -6,16 +6,36 @@ namespace Assets.Scripts
     public class PlayerMovement : MonoBehaviour
     {
         public float speed = 5f;
+        public float runSpeedMultiplier = 1.5f;
         public float gravity = -9.81f;
         public float jumpHeight = 1.5f;
+
+        [Header("Sound Settings")]
+        [Tooltip("Sound intensity when walking")]
+        public float walkSoundIntensity = 0.5f;
+        [Tooltip("Sound intensity when running (shift)")]
+        public float runSoundIntensity = 0.7f;
+        [Tooltip("Sound intensity when jumping")]
+        public float jumpSoundIntensity = 0.8f;
+        [Tooltip("Interval between footstep sounds (seconds)")]
+        public float footstepInterval = 0.5f;
+        [Tooltip("Generate sounds")]
+        public bool emitSounds = true;
 
         private CharacterController controller;
         private Vector3 velocity;
         private bool isGrounded;
+        private float footstepTimer = 0f;
+
+        private const float MovementThresholdSqr = 0.01f;
+        private float runSpeed;
+        private float jumpVelocity;
 
         void Start()
         {
             controller = GetComponent<CharacterController>();
+            runSpeed = speed * runSpeedMultiplier;
+            jumpVelocity = Mathf.Sqrt(jumpHeight * -2f * gravity);
         }
 
         void Update()
@@ -30,11 +50,33 @@ namespace Assets.Scripts
             float z = Input.GetAxis("Vertical");
 
             Vector3 move = transform.right * x + transform.forward * z;
-            controller.Move(move * speed * Time.deltaTime);
+
+            bool isMoving = move.sqrMagnitude > MovementThresholdSqr;
+
+            bool isRunning = Input.GetKey(KeyCode.LeftShift);
+            float currentSpeed = isRunning ? runSpeed : speed;
+
+            controller.Move(move * currentSpeed * Time.deltaTime);
+
+            if (emitSounds && isMoving && isGrounded)
+            {
+                footstepTimer -= Time.deltaTime;
+                if (footstepTimer <= 0f)
+                {
+                    float soundIntensity = isRunning ? runSoundIntensity : walkSoundIntensity;
+                    SoundManager.EmitSound(transform.position, soundIntensity, gameObject);
+                    footstepTimer = footstepInterval;
+                }
+            }
 
             if (Input.GetButtonDown("Jump") && isGrounded)
             {
-                velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+                velocity.y = jumpVelocity;
+
+                if (emitSounds)
+                {
+                    SoundManager.EmitSound(transform.position, jumpSoundIntensity, gameObject);
+                }
             }
 
             velocity.y += gravity * Time.deltaTime;
