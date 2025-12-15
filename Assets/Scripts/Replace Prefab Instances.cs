f48f4ccf738f2b208a29100f4daeae877c31c034
Paziloi_4elovek <m.des1gn3037@gmail.com>
2025-12-15 13:25:42 +0200

    Upgrading
    Fix tree, slulpting and place some water, create new scene

---
+++ Assets/Scripts/Replace Prefab Instances.cs
@@ -0,0 +1,88 @@
+using UnityEngine;
+using UnityEditor;
+using System.Reflection;
+
+public class PrefabReplacerAdvanced : EditorWindow
+{
+    private GameObject oldPrefab;
+    private GameObject newPrefab;
+
+    [MenuItem("Tools/Prefab Replacer Advanced")]
+    public static void ShowWindow()
+    {
+        GetWindow<PrefabReplacerAdvanced>("Prefab Replacer Advanced");
+    }
+
+    private void OnGUI()
+    {
+        GUILayout.Label("Replace Prefabs and keep component overrides", EditorStyles.boldLabel);
+
+        oldPrefab = (GameObject)EditorGUILayout.ObjectField("Old Prefab", oldPrefab, typeof(GameObject), false);
+        newPrefab = (GameObject)EditorGUILayout.ObjectField("New Prefab", newPrefab, typeof(GameObject), false);
+
+        if (GUILayout.Button("Replace Prefab Instances"))
+        {
+            if (oldPrefab == null || newPrefab == null)
+            {
+                EditorUtility.DisplayDialog("Error", "Please assign both Old Prefab and New Prefab!", "OK");
+            }
+            else
+            {
+                ReplacePrefabs();
+            }
+        }
+    }
+
+    private void ReplacePrefabs()
+    {
+        GameObject[] allObjects = GameObject.FindObjectsOfType<GameObject>();
+        int count = 0;
+
+        foreach (GameObject go in allObjects)
+        {
+            GameObject prefabRoot = PrefabUtility.GetCorrespondingObjectFromSource(go);
+            if (prefabRoot == oldPrefab)
+            {
+                // Сохраняем Transform
+                Vector3 pos = go.transform.position;
+                Quaternion rot = go.transform.rotation;
+                Vector3 scale = go.transform.localScale;
+                Transform parent = go.transform.parent;
+
+                // Сохраняем компоненты и их поля (локальные модификации)
+                Component[] components = go.GetComponents<Component>();
+                
+                // Создаем новый Prefab
+                GameObject newObj = (GameObject)PrefabUtility.InstantiatePrefab(newPrefab);
+                newObj.transform.position = pos;
+                newObj.transform.rotation = rot;
+                newObj.transform.localScale = scale;
+                newObj.transform.parent = parent;
+
+                // Копируем поля компонентов (локальные изменения)
+                foreach (Component comp in components)
+                {
+                    if (comp == null) continue;
+                    System.Type type = comp.GetType();
+                    Component newComp = newObj.GetComponent(type);
+                    if (newComp != null)
+                    {
+                        FieldInfo[] fields = type.GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+                        foreach (FieldInfo field in fields)
+                        {
+                            if (field.IsDefined(typeof(SerializeField), true) || field.IsPublic)
+                            {
+                                field.SetValue(newComp, field.GetValue(comp));
+                            }
+                        }
+                    }
+                }
+
+                DestroyImmediate(go);
+                count++;
+            }
+        }
+
+        EditorUtility.DisplayDialog("Prefab Replacer", $"Replaced {count} instance(s)!", "OK");
+    }
+}
