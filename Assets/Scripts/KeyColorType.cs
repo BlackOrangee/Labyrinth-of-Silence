f92fba15d328d78816f02e0206253130152cef2a
BlackOrangee <33674663+BlackOrangee@users.noreply.github.com>
2026-01-24 14:33:35 +0200

    inventory UI update

--- Assets/Scripts/KeyColorType.cs
+++ Assets/Scripts/KeyColorType.cs
@@ -6,6 +6,23 @@ namespace Assets.Scripts
         Green,
         Yellow,
         Blue,
-        Pink
+        Pink,
+
+        // Virtual keys (не отображаются в инвентаре, но проверяются системой)
+        GeneratorPower = 100,
+        Electricity = 101,
+        WaterValve = 102,
+        SecurityAccess = 103
+    }
+
+    public static class KeyColorTypeExtensions
+    {
+        /// <summary>
+        /// Проверяет, является ли ключ виртуальным (не отображается в UI)
+        /// </summary>
+        public static bool IsVirtual(this KeyColorType keyType)
+        {
+            return (int)keyType >= 100;
+        }
     }
 }
